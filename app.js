ee2bd4cb54bbf41d10a62a4ba32e46cc143913ff
Morgan Harmon <morganharmon@gmail.com>
2022-05-05 21:16:48 -0700

    add lose condition and resetGame

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ import { renderGoblin } from './utils.js';
 const goblinArea = document.getElementById('goblinArea');
 const form = document.getElementById('form');
 const goblinsDestroyedDiv = document.getElementById('goblinsDestroyedDiv');
+const hpDiv = document.getElementById('yourHP');
+const resetButton = document.getElementById('reset');
 
 // let state
 let goblinsDestroyed = 0;
+goblinsDestroyedDiv.textContent = 0;
 let goblins = [
     { id: 1, name: 'Horace', HitPoints: 5 },
     { id: 2, name: 'Gladys', HitPoints: 3 }
 ];
 let HP = 10;
+hpDiv.textContent = 10;
 let currentID = 3;
-goblinsDestroyedDiv.textContent = goblinsDestroyed;
 
 // set event listeners 
 form.addEventListener('submit', (e) => {
@@ -22,7 +25,7 @@ form.addEventListener('submit', (e) => {
     const newGuy = {
         id: currentID,
         name: name,
-        HitPoints: Math.ceil(Math.random() * 5)
+        HitPoints: 3
     };
     currentID++;
     goblins.push(newGuy);
@@ -33,7 +36,6 @@ form.addEventListener('submit', (e) => {
 function displayGoblins() {
     goblinArea.textContent = '';
     for (let goblin of goblins) {
-        console.log(goblin);
         const gobbo = renderGoblin(goblin);
         gobbo.addEventListener('click', () => {
             goblinClickHandler(goblin);
@@ -43,24 +45,48 @@ function displayGoblins() {
 }
 
 function goblinClickHandler(gobbo) {
-    if (gobbo.HitPoints === 0) {
-        return;
-    }
+    if (gobbo.HitPoints === 0) return;
     const num = Math.ceil(Math.random() * 10);
-    if (num < 5) {
+    if (num < 7) {
         alert(`${gobbo.name} hits you for 1 damage!`);
         HP--;
+        hpDiv.textContent = HP;
+        if (HP === 0) {
+            alert('The goblins have prevailed. You lose. Don\'t do drugs.');
+            resetButton.classList.toggle('hide');
+            form.classList.toggle('hide');
+            goblinArea.classList.toggle('hide');
+        }
     } else {
         alert(`You hit ${gobbo.name} for 1 damage!`);
         gobbo.HitPoints--;
-    }
-    if (gobbo.hp === 0) {
-        alert(`You killed ${gobbo.name}! Their gold is yours.`);
-        goblinsDestroyed++;
-    }
-    if (HP === 0) {
-        alert('The goblins have prevailed. You lose. Don\'t do drugs.');
+        if (gobbo.HitPoints === 0) {
+            alert(`You killed ${gobbo.name}! Their gold is yours.`);
+            goblinsDestroyed++;
+            goblinsDestroyedDiv.textContent = goblinsDestroyed;
+
+        }
     }
     displayGoblins();
 }
 
+resetButton.addEventListener('click', () => {
+    resetGame();
+});
+
+function resetGame() {
+    HP = 10;
+    hpDiv.textContent = 10;
+    goblins = [
+        { id: 1, name: 'Horace', HitPoints: 5 },
+        { id: 2, name: 'Gladys', HitPoints: 3 }
+    ];
+    resetButton.classList.toggle('hide');
+    form.classList.toggle('hide');
+    goblinArea.classList.toggle('hide');
+    goblinsDestroyed = 0;
+    goblinsDestroyedDiv.textContent = 0;
+    displayGoblins();
+}
+        
+displayGoblins();
--- utils.js
+++ utils.js
@@ -4,14 +4,14 @@ export function renderGoblin(gobbo) {
     const hp = document.createElement('p');
     
     newGobbo.classList.add('goblin');
-    name.textContent = gobbo.name;
+    name.textContent = gobbo.name || (gobbo.name = `spawn#${Math.ceil(Math.random() * 100)}`);
     hp.textContent = gobbo.HitPoints;
-    hp.id = `${gobbo.id}-hp`;
+    // hp.id = `${gobbo.id}-hp`;
     
     newGobbo.append(name, hp);
-
-    if (gobbo.HitPoints < 1) {
-        gobbo.classList.add('dead');
+    
+    if (gobbo.HitPoints === 0) {
+        newGobbo.classList.add('dead');
     }
 
     return newGobbo;
